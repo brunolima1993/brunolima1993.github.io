b067a5c72f5977c271e50f720cb260f4bd600b03
brunolima1993 <brunolimaa.eng@gmail.com>
2026-08-29 19:29:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_APP = 'tmycar-pwa-v1.5.57';
+const CACHE_APP = 'tmycar-pwa-v1.5.58';
 const INICIO = new URL('./', self.registration.scope).href;
 const HTML_PRINCIPAL = new URL('./index.html', self.registration.scope).href;
 const ARQUIVOS_APP = [
